c06df787b615cf96c0ee9b434fac0e4d0b28b34c
Solène Rambaut <rambaut.solene@yahoo.fr>
2019-03-21 17:49:59 +0100

    ajout favicon

--- socks.php
+++ socks.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+    <link rel="shortcut icon" type="image/png" href="../images/favicon.ico"/>
     <link href="https://fonts.googleapis.com/css?family=Merienda+One%7CPermanent+Marker%7CRoboto%7CSource+Sans+Pro%7CRaleway" rel="stylesheet">
     <title>Chaussettes - Wild Bazar</title>
 </head>
